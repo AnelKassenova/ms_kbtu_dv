fdd4b3cd717f1a5428ecf6a7ca60d329d2794215
AnelKassenova <113194582+AnelKassenova@users.noreply.github.com>
2022-10-26 22:58:21 +0600

    Update chart.js

--- task2/chart.js
+++ task2/chart.js
@@ -1,50 +1,114 @@
-async function buildPlot() {
-    console.log("Hello world");
-    const data = await d3.json("my_weather_data.json");
-    //console.table(data);
-    const dateParser = d3.timeParse("%Y-%m-%d");
-    const yAccessor = (d) => d.temperatureMin;
-    const xAccessor = (d) => dateParser(d.date);
-    // Функции для инкапсуляции доступа к колонкам набора данных
+async function drawBar(buttoni, temp) {
 
-    var dimension = {
-        width: window.innerWidth*0.9,
-        height: 400,
+    const dataset = await d3.json("./my_weather_data.json")
+    //Accessor
+    const xAccessor = d => d[temp];
+    const yAccessor = d => d.length;
+    console.log(yAccessor(temp))
+
+    const width = 600
+    let dimensions = {
+        width: width,
+        height: width * 0.6,
         margin: {
-            top: 15,
-            left: 15,
-            bottom: 15,
-            right: 15
-        }
-    };
+            top: 20,
+            right: 30,
+            bottom: 20,
+            left: 30,
+        },
+    }
+    dimensions.boundedWidth = dimensions.width
+        - dimensions.margin.left
+        - dimensions.margin.right
+    dimensions.boundedHeight = dimensions.height
+        - dimensions.margin.top
+        - dimensions.margin.bottom
 
-    dimension.boundedWidth = dimension.width - dimension.margin.left - dimension.margin.right;
-    dimension.boundedHeight = dimension.height - dimension.margin.top - dimension.margin.bottom;
+    // 3. Draw canvas
 
-    const wrapper = d3.select("#wrapper");
-    const svg = wrapper.append("svg")
-    svg.attr("height",dimension.height);
-    svg.attr("width",dimension.width);
-    const bounded = svg.append("g");
-    bounded.style("transform",`translate(${dimension.margin.left}px, ${dimension.margin.top})`);
+    const wrapper = d3.select("#wrapper")
+        .html("") 
+        .append("svg")
+        .attr("width", dimensions.width)
+        .attr("height", dimensions.height);
+
+    const bounds = wrapper.append("g")
+        .style("translate", `translate(${dimensions.margin.left}px,${dimensions.margin.top}px)`);
+
+    const xScaler = d3.scaleLinear()
+        .domain(d3.extent(dataset, xAccessor))
+        .range([0, dimensions.boundedWidth])
+        .nice()
+
+    const binsGen = d3.bin()
+        .domain(xScaler.domain())
+        .value(xAccessor)
+        .thresholds(12);
+
+    const bins = binsGen(dataset);
+    console.log(bins);
 
     const yScaler = d3.scaleLinear()
-        .domain(d3.extent(data,yAccessor))
-        .range([dimension.boundedHeight,0]);
+        .domain([0, d3.max(bins, yAccessor)])
+        .range([dimensions.boundedHeight, 0])
 
-    const xScaler = d3.scaleTime()
-        .domain(d3.extent(data,xAccessor))
-        .range([0,dimension.boundedWidth]);
+    const binGroup = bounds.append("g");
+    const binGroups = binGroup.selectAll("g")
+        .data(bins)
+        .enter()
+        .append("g");
 
-    var lineGenerator = d3.line()
-        .x(d => xScaler(xAccessor(d)))
-        .y(d => yScaler(yAccessor(d)));
 
-    bounded.append("path")
-        .attr("d",lineGenerator(data))
-        // .attr("fill","none")
-        .attr("stroke","lightgrey")
+    const margin = 40;
+    const barPadding = 1
+    const barRect = binGroups.append("rect")
+        .attr("x", d => xScaler(d.x0) + barPadding / 2 + margin) 
+        .attr("y", d => yScaler(yAccessor(d)))
+        .attr("width", d => d3.max([0, xScaler(d.x1) - xScaler(d.x0) - barPadding]))
+        .attr("height", d => dimensions.boundedHeight - yScaler(yAccessor(d)))
+        .attr("fill", "red");
+
+    const mean = d3.mean(dataset, xAccessor);
+    console.log(mean);
+    const meanLine = bounds.append("line")
+        .attr("x1", xScaler(mean))
+        .attr("x2", xScaler(mean))
+        .attr("y1", -15)
+        .attr("y2", dimensions.boundedHeight)
+        .attr("stroke", "black")
+        .attr("stroke-dasharray", "2px 4px");
+
+    const meanLabel = bounds.append("text")
+        .attr("x", xScaler(mean))
+        .attr("y", 10)
+        .text("Mean")
+        .attr("fill", "blue")
+        .attr("font-size", "12px")
+        .attr("text-anchor", "middle");
+
+    const xAxisGen = d3.axisBottom()
+        .scale(xScaler);
+    const xAxis = bounds.append("g")
+        .call(xAxisGen)
+        .attr("transform", `translate(${margin},${dimensions.boundedHeight} )`);
+
+
+    const yAxisGen = d3.axisLeft()
+        .scale(yScaler);
+    const yAxis = bounds.append("g")
+        .call(yAxisGen)
+        .attr("transform", `translate(${margin}, 0)`);
+
+    
+    const barText = binGroups.filter(yAccessor)
+        .append("text")
+        .attr("x", d => xScaler(d.x0) + (xScaler(d.x1) - xScaler(d.x0)) / 2)
+        .attr("y", d => yScaler(yAccessor(d)) - 5)
+        .text(yAccessor)
+        .attr("fill", "darkgrey")
+        .attr("font-size", "12px")
+        .attr("text-anchor", "middle");
 
 }
 
-buildPlot();
+drawBar();
